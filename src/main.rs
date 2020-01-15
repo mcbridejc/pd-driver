83c3fa9e3604f60a482d786601584da0750d213a
Jeff McBride <mcbridejc@gmail.com>
2020-01-14 21:00:17 -0800

    Setup PLL and enable TX buffer

--- src/main.rs
+++ src/main.rs
@@ -39,15 +39,16 @@ const APP: () = {
     struct Resources {
         // A resource
         uart: hal::serial::Serial<hal::stm32::USART3, (gpiod::PD8<Alternate<AF7>>, gpiod::PD9<Alternate<AF7>>)>,
-        //uart2_rx: hal::serial::Rx<hal::stm32::USART2>,
         uart_rx_producer: Producer<'static, u8, U512>,
         uart_rx_consumer: Consumer<'static, u8, U512>,
+        uart_tx_producer: Producer<'static, u8, U512>,
+        uart_tx_consumer: Consumer<'static, u8, U512>,
     }
 
     #[init]
     fn init(cx: init::Context) -> init::LateResources {
-        static mut X: u32 = 0;
-        static mut UART_BUFFER: Queue<u8, U512> = Queue(i::Queue::new());
+        static mut UART_RX_BUFFER: Queue<u8, U512> = Queue(i::Queue::new());
+        static mut UART_TX_BUFFER: Queue<u8, U512> = Queue(i::Queue::new());
 
         // Cortex-M peripherals
         let core: cortex_m::Peripherals = cx.core;
@@ -55,10 +56,17 @@ const APP: () = {
         // Device specific peripherals
         let device: target_device::Peripherals = cx.device;
 
-        let mut rcc = device.RCC.constrain();
-        // 
-        // clock configuration using the default settings (all clocks run at 8 MHz)
-        let clocks = rcc.cfgr.freeze();
+        let mut rcc = device.RCC;
+        // Configure PLL settings
+        // Using 16Mhz HSI oscillator as input, setting system and APB to 100MHz, and AHB to 50Mhz is the goal
+        rcc.pllcfgr.write(|w| unsafe { w.pllm().bits(8).plln().bits(50).pllp().div2().pllsrc().hsi() });
+        // enable PLL
+        rcc.cr.modify(|_r, w| w.pllon().set_bit());
+        // wait for PLL to become ready
+        while rcc.cr.read().pllrdy().bit_is_clear() {}
+        // Divide AHB by two to get 50MHz
+        rcc.cfgr.modify(|_r, w| { w.ppre1().div2().sw().hsi() });
+        let clocks = rcc.constrain().cfgr.freeze();
         
         let mut gpiod = device.GPIOD.split();
         let tx_pin = gpiod.pd8.into_alternate_af7();
@@ -69,8 +77,9 @@ const APP: () = {
             hal::serial::config::Config::default().baudrate(115_200.bps()),
             clocks,
         ).unwrap();
-        //let (uart2_tx, uart2_rx) = uart2.split();
-        let (uart_rx_producer, uart_rx_consumer) = UART_BUFFER.split();
+        
+        let (uart_rx_producer, uart_rx_consumer) = UART_RX_BUFFER.split();
+        let (uart_tx_producer, uart_tx_consumer) = UART_TX_BUFFER.split();
         
         let mut systick = core.SYST;
         // systick.set_clock_source(systick::SystClkSource::Core);
@@ -86,10 +95,10 @@ const APP: () = {
 
         rtfm::pend(Interrupt::USART3);
 
-        init::LateResources { uart, uart_rx_producer, uart_rx_consumer }
+        init::LateResources { uart, uart_rx_producer, uart_rx_consumer, uart_tx_producer, uart_tx_consumer }
     }
 
-    #[idle(resources=[uart, uart_rx_consumer])]
+    #[idle(resources=[uart, uart_tx_producer, uart_rx_consumer])]
     fn idle(mut cx: idle::Context) -> ! {
         static mut X: u32 = 0;
 
@@ -102,23 +111,34 @@ const APP: () = {
 
         loop {
             match cx.resources.uart_rx_consumer.dequeue() {
-                Some(x) => cx.resources.uart.lock(|uart| {
-                    hal::block!(uart.write(x)).unwrap();
-                }),
+                Some(x) => {
+                    cx.resources.uart_tx_producer.enqueue(x);
+                    cx.resources.uart.lock(|uart| {
+                        uart.listen(hal::serial::Event::Txe);
+                    });
+                },
                 None => (),
             };
         }
     }
 
     // `shared` can be accessed from this context
-    #[task(binds = USART3, resources = [uart, uart_rx_producer])]
+    #[task(binds = USART3, resources = [uart, uart_rx_producer, uart_tx_consumer])]
     fn uart3(cx: uart3::Context) {
-        // let shared: &mut u32 = cx.resources.shared;
-        // *shared += 1;
-        match cx.resources.uart.read() {
-            Ok(x) => cx.resources.uart_rx_producer.enqueue(x).expect("rx Overflow"),
-            Err(_) => (),
-        };
+
+        if cx.resources.uart.is_rxne() {
+            match cx.resources.uart.read() {
+                Ok(x) => cx.resources.uart_rx_producer.enqueue(x).expect("rx Overflow"),
+                Err(_) => (),
+            };
+        }
+
+        if cx.resources.uart.is_txe() {
+            match cx.resources.uart_tx_consumer.dequeue() {
+                Some(x) => cx.resources.uart.write(x).unwrap(),
+                None => cx.resources.uart.unlisten(hal::serial::Event::Txe),
+            }
+        }
 
         hprintln!("UART2").unwrap(); //: shared = {}", shared).unwrap();
     }
